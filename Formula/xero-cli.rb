e1f9af31974d228608d68615601615571925f248
Sion Smith <sion.smith@gmail.com>
2026-03-02 09:46:08 +0000

    xero-cli 0.3.0

---
+++ Formula/xero-cli.rb
@@ -0,0 +1,29 @@
+class XeroCli < Formula
+  desc "A fast, intelligent CLI for the Xero Accounting API"
+  homepage "https://github.com/osodevops/xero-cli"
+  version "0.3.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/osodevops/xero-cli/releases/download/v0.3.0/xero-aarch64-apple-darwin.tar.gz"
+      sha256 "794a11f66e94bf06a8879755df762ea72ae77273398504657b9bc13098b02c3e"
+    else
+      url "https://github.com/osodevops/xero-cli/releases/download/v0.3.0/xero-x86_64-apple-darwin.tar.gz"
+      sha256 "23464759e52ee20fecac6c85a86d802f14522a7658b5bfc6ac9600d364756c1d"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/osodevops/xero-cli/releases/download/v0.3.0/xero-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "04b41530b161f469c3d8ba9bd2daecd56f9b9dda08de3a8d38ea46bb90732949"
+  end
+
+  def install
+    bin.install "xero"
+  end
+
+  test do
+    assert_match "xero #{version}", shell_output("#{bin}/xero --version")
+  end
+end
