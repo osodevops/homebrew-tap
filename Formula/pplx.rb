79ff60286492b163377e39f33e4ad81e8a0a5914
Sion Smith <sion.smith@gmail.com>
2026-03-02 09:42:28 +0000

    pplx 0.3.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/pplx.rb
+++ Formula/pplx.rb
@@ -1,13 +1,13 @@
 class Pplx < Formula
   desc "A fast Perplexity API CLI built in Rust"
   homepage "https://github.com/osodevops/perplexity-cli"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/osodevops/perplexity-cli/releases/download/v#{version}/pplx-aarch64-apple-darwin.tar.gz"
-      sha256 "91b041daaf3e0587c80ac6580d23ea0cb062b89897667f17638c244ab6568d1a"
+      url "https://github.com/osodevops/perplexity-cli/archive/refs/tags/v0.3.0.tar.gz"
+      sha256 "e5c1ed1c94b94e1ab27e43c7c21c219d98604c47561009157b59e7b5b060d6f9"
     else
       url "https://github.com/osodevops/perplexity-cli/releases/download/v#{version}/pplx-x86_64-apple-darwin.tar.gz"
       sha256 "69f2c54d71dd7799280ba7e7f791e9a04af6c5235e23924e801336ccc71babb5"
