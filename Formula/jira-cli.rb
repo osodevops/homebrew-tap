f65d7fe453ca7fa31e2b0203b62c47ec854e858e
Sion Smith <sion.smith@gmail.com>
2026-05-27 20:49:23 +0100

    Fix jira-cli formula

--- Formula/jira-cli.rb
+++ Formula/jira-cli.rb
@@ -1,20 +1,15 @@
 class JiraCli < Formula
   desc "Fast, agent-friendly CLI for Atlassian Jira Cloud"
   homepage "https://github.com/osodevops/jira-cli"
-  version "0.1.0"
   license "MIT"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/osodevops/jira-cli/releases/download/v0.1.0/jira-aarch64-apple-darwin.tar.gz"
-      sha256 "18eb4ea02544d3f47ffe7006fa118f1b50676481f6d85805d4cdbacddd714206"
-    else
-      url "https://github.com/osodevops/jira-cli/releases/download/v0.1.0/jira-x86_64-apple-darwin.tar.gz"
-      sha256 "c52b29c26aa5ac8dc6bd2e2df52406b0aaf1de8dfdef7695326ab25fbecdbd47"
-    end
-  end
-
-  on_linux do
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/osodevops/jira-cli/releases/download/v0.1.0/jira-aarch64-apple-darwin.tar.gz"
+    sha256 "18eb4ea02544d3f47ffe7006fa118f1b50676481f6d85805d4cdbacddd714206"
+  elsif OS.mac?
+    url "https://github.com/osodevops/jira-cli/releases/download/v0.1.0/jira-x86_64-apple-darwin.tar.gz"
+    sha256 "c52b29c26aa5ac8dc6bd2e2df52406b0aaf1de8dfdef7695326ab25fbecdbd47"
+  elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/osodevops/jira-cli/releases/download/v0.1.0/jira-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "57e9bad7f3018b2a6136ccd1420d04a54cfbd1b04bb9f4e09dd92e2c065bbb41"
   end
